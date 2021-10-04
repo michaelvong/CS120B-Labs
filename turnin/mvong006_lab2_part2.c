7504f0097765c8fa7a33ff07d178882795db78f5
Michael <michaelvong100@gmail.com>
2021-10-03 20:47:05 -0700

    Updated part 2

--- turnin/mvong006_lab2_part2.c
+++ turnin/mvong006_lab2_part2.c
@@ -18,12 +18,11 @@ int main(void) {
     PORTA = 0xFF;
     DDRB = 0xFF;
     PORTB = 0x00;
-    unsigned char tmpB = 0x00;
     unsigned char tmpA = 0x00;
     /* Insert your solution below */
     while (1) {
 	tmpA = PINA;
-	if (tmpA == 0x00 || tmpA = 0x80){
+	if (tmpA == 0x00 || tmpA == 0x80){
 		PORTC = 0x04;
 	}
 	else if (tmpA == 0x01 || tmpA == 0x02 || tmpA == 0x04 || tmpA == 0x08){
@@ -32,7 +31,7 @@ int main(void) {
 	else if (tmpA == 0x03 || tmpA == 0x05 || tmpA == 0x06 || tmpA == 0x09 || tmpA == 0x0A || tmpA == 0x0C){
 		PORTC = 0x02;
 	}
-	else if (tmpA = 0x07 || tmpA == 0x0B || tmpA == 0x0D || tmpA == 0x0E){
+	else if (tmpA == 0x07 || tmpA == 0x0B || tmpA == 0x0D || tmpA == 0x0E){
 		PORTC = 0x01;
 	}
 	else{
